5b1f6072f864ea2dc0c46089d188c2c5541f7b8f
Juan Sebastian Giraldo <juans.giraldo.e@gmail.com>
2024-03-02 14:54:23 -0500

    [Update] - Validate the form. // Display error message in the DOM.

--- js/app.js
+++ js/app.js
@@ -12,6 +12,30 @@ function buscarClima(e) {
   e.preventDefault();
   
   //Validacion del formulario.
+  const ciudad = document.querySelector('#ciudad').value;
+  const pais = document.querySelector('#pais').value;
+
+  if (ciudad === '' || pais === '') {
+    mostrarError('Ambos campos son obligatorios.');
+    return
+  };
 
   //Consultar la API.
+};
+
+function mostrarError(msj) {
+  const alerta = document.querySelector('.error');
+  if (!alerta) {
+    const alerta = document.createElement('DIV');
+    alerta.classList.add('bg-red-100', 'border-red-400', 'text-red-700', 'px-4', 'py-3', 'rounded', 'max-w-md', 'mx-auto', 'mt-6', 'text-center', 'error');
+    alerta.innerHTML = `
+    <strong class='font-bold'>¡Error!</strong>
+    <span class='block'>${msj}</span>
+    `;
+    container.appendChild(alerta);
+    setTimeout(() => {
+      alerta.remove();
+    }, 2000);
+  };
+
 };
